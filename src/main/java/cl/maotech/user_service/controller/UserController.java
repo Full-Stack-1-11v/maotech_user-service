75474990f71299a3485e0ebab08bb2f1777fde76
Alex Caica <al.caica@duocuc.cl>
2025-05-23 22:31:19 -0400

    feature: actualización en clase User, UserController con Endpoints probados en DB local

--- src/main/java/cl/maotech/user_service/controller/UserController.java
+++ src/main/java/cl/maotech/user_service/controller/UserController.java
@@ -1,11 +1,87 @@
 package cl.maotech.user_service.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import cl.maotech.user_service.model.User;
+import cl.maotech.user_service.service.UserService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
 @RestController
-@RequestMapping("/api/v1/user")
+@RequestMapping("/api/v1/users")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/create")
+    public ResponseEntity<User> create(@RequestBody User user) {
+        User newUser = userService.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<User>> list() {
+        List<User> users = userService.findAll();
+        if (users.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.ok(users);
+        }
+    }
+
+    @GetMapping("/{id}/details")
+    public ResponseEntity<User> findById(@PathVariable Integer id) {
+        try {
+            User user = userService.findById(id);
+            return ResponseEntity.ok(user);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+                }
+    }
+
+    @PutMapping("/{id}/update")
+    public ResponseEntity<User> update(@PathVariable Integer id, @RequestBody User user) {
+        try {
+            User up_user = userService.findById(id);
+            up_user.setUser_id(id);
+            up_user.setRut(user.getRut());
+            up_user.setFirst_name(user.getFirst_name());
+            up_user.setLast_name(user.getLast_name());
+            up_user.setEmail(user.getEmail());
+            up_user.setStatus(user.getStatus());
+            up_user.setRole_id(user.getRole_id());
+
+            userService.save(up_user);
+            return ResponseEntity.ok(up_user);
+
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }        
+    }
+
+    @DeleteMapping("/{id}/delete")
+    public ResponseEntity<?> delete(@PathVariable Integer id) {
+        try {
+            userService.delete(id);
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    
+    
 }
--- src/main/java/cl/maotech/user_service/model/User.java
+++ src/main/java/cl/maotech/user_service/model/User.java
@@ -1,7 +1,5 @@
 package cl.maotech.user_service.model;
 
-import java.sql.Date;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -23,7 +21,13 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer user_id;
 
-    @Column(unique = true, length = 13, nullable = false)
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true, length = 12, nullable = false)
     private String rut;
 
     @Column(nullable = false)
@@ -33,18 +37,9 @@ public class User {
     private String last_name;
 
     @Column(nullable = false)
-    private Date birthdate;
-
-    @Column(nullable = false)
-    private String email;
-
-    @Column(nullable = false)
-    private String password;
-
-    @Column(nullable = false)
     private Boolean status;
 
     @Column(nullable = false)
-    private Integer id_rol;
+    private Integer role_id;
 
 }
