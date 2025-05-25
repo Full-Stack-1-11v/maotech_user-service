1e806babc4765a6196388bbe2754096e1ac4f2d3
Alex Caica <al.caica@duocuc.cl>
2025-05-25 02:34:07 -0400

    feature: Se agregan archivos DTO para implementar

---
+++ src/main/java/cl/maotech/user_service/dto/UserEditDTO.java
@@ -0,0 +1,25 @@
+package cl.maotech.user_service.dto;
+
+import cl.maotech.user_service.model.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEditDTO {
+
+    private Integer userId;
+
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+    private Boolean status;
+
+    private Role role;
+
+}
