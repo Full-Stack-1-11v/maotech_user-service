5ff2626e5390eda850a61b361b2cddb0b6a5ccf6
Alex Caica <al.caica@duocuc.cl>
2025-06-25 23:26:19 -0400

    ADD: Configuración Swagger

---
+++ src/main/java/cl/maotech/user_service/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package cl.maotech.user_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title("User Service API")
+                        .version("1.0")
+                        .description("Documentación de la API para la gestión de usuarios en el sistema Edutech"));
+    }
+}
